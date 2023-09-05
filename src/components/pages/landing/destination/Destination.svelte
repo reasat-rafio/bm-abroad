e1516f7877649b7033882264eef867bddc209fe3
reasat-rafio <rafioar96@gmail.com>
2023-09-05 18:03:06 +0600

    :iphone: Destination responsiveness update

--- src/components/pages/landing/destination/Destination.svelte
+++ src/components/pages/landing/destination/Destination.svelte
@@ -8,26 +8,34 @@
     type EmblaOptionsType,
     type EmblaPluginType,
   } from 'embla-carousel-svelte';
-  import { slide } from 'svelte/transition';
+  import { fade, slide } from 'svelte/transition';
+  import Autoplay from 'embla-carousel-autoplay';
 
   export let props: DestinationsProps;
   $: ({ title, subtitle, destinations } = props);
 
   let emblaApi: EmblaCarouselType;
-  let plugins: EmblaPluginType[] = [];
+  let plugins: EmblaPluginType[] = [Autoplay()];
   let options: Partial<EmblaOptionsType> = { align: 'start' };
+  let activeSlide = 0;
+  let hoverdCardKey: null | string = null;
+  let windowWidth = 0;
 
   const onInit = (event: CustomEvent<EmblaCarouselType>) => {
     emblaApi = event.detail;
   };
 
-  let hoverdCardKey: null | string = null;
-  $: {
-    console.log(hoverdCardKey);
+  $: isSm = windowWidth < 768 ? true : false;
+  $: activeSlideDescription = destinations[activeSlide]?.description;
+  $: if (emblaApi) {
+    emblaApi.on('select', ({ selectedScrollSnap }) => {
+      activeSlide = selectedScrollSnap();
+    });
   }
 </script>
 
-<section class="mb-2xl">
+<svelte:window bind:innerWidth={windowWidth} />
+<section class="mb-[90px] md:mb-2xl">
   <div class="container">
     <SectionHeader {title} {subtitle} />
 
@@ -36,12 +44,13 @@
       use:emblaCarouselSvelte={{ plugins, options }}
       on:emblaInit={onInit}
     >
-      <div class="relative flex space-x-[10px] md:space-x-[24px]">
+      <div class="relative flex space-x-[20px] md:space-x-[24px]">
         {#each destinations as { image, description, name, _key }}
+          <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
           <article
             on:mouseenter={() => (hoverdCardKey = _key)}
             on:mouseleave={() => (hoverdCardKey = null)}
-            class="relative h-[512px] overflow-hidden rounded-md"
+            class="relative block h-[376px] flex-[0_0_calc(95%-24px)] overflow-hidden rounded-md md:h-[512px] md:flex-[0_0_calc(50%-24px)] xl:flex-[0_0_calc(33.33%-20px)] 2xl:flex-[0_0_calc(25%-20px)]"
           >
             <SanityImage
               class="h-full w-full object-cover"
@@ -51,11 +60,11 @@
               imageUrlBuilder={imageBuilder}
             />
             <div
-              style="background: {hoverdCardKey === _key
+              style="background: {hoverdCardKey === _key && !isSm
                 ? 'linear-gradient(180deg, rgba(3, 3, 3, 0.40) 0%, #502CB5 100%)'
                 : ''};"
               class="absolute inset-0 flex h-full w-full flex-col items-center justify-end p-[35px] text-white transition-all duration-300 {hoverdCardKey ===
-              _key
+                _key && !isSm
                 ? 'backdrop-blur-lg'
                 : ''}"
             >
@@ -64,7 +73,7 @@
               >
                 {name}
               </h5>
-              {#if hoverdCardKey === _key}
+              {#if hoverdCardKey === _key && !isSm}
                 <p
                   transition:slide={{ duration: 600 }}
                   class="mt-[30%] text-center"
@@ -76,6 +85,18 @@
           </article>
         {/each}
       </div>
+    </div>
+
+    <div class="md:hidden">
+      {#key activeSlide}
+        <p
+          in:fade={{ duration: 600 }}
+          out:fade={{ duration: 0 }}
+          class="mt-[32px] text-[16px] font-light leading-[160%] tracking-[0.4px]"
+        >
+          {activeSlideDescription}
+        </p>
+      {/key}
     </div>
   </div>
 </section>
