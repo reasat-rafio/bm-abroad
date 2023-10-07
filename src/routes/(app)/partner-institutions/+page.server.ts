28b58522f30c0df546bcd7b754aec1da5d804a4c
Al Reasat Rafio <rafioar96@gmail.com>
2023-10-07 18:58:36 +0600

    :alien: Partner institutions page query added

---
+++ src/routes/(app)/partner-institutions/+page.server.ts
@@ -0,0 +1,30 @@
+import { asset } from '@/lib/sanity/sanity-image';
+import { sanityClient } from '@/lib/sanity/sanityClient';
+import { error, type ServerLoad } from '@sveltejs/kit';
+import groq from 'groq';
+
+const query = groq`*[_id == "partnerInstitutionsPage"][0]{
+    ...,
+    sections[]{
+        ...,
+        ${asset('image')},
+        reasons[]{
+            ...,
+            ${asset('image')},
+        },
+        universities[]{
+            ...,
+            ${asset('coverPhoto')},
+            ${asset('logo')},
+        }
+    }
+}`;
+
+export const load: ServerLoad = async () => {
+  const data = await sanityClient.fetch(query);
+  if (!data) throw error(404, { message: 'Not found' });
+
+  return {
+    page: data,
+  };
+};
