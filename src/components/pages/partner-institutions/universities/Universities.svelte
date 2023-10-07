0ae4efc46fffdcd416ba87b0b566a4b44ef0e559
reasat-rafio <rafioar96@gmail.com>
2023-10-07 21:03:41 +0600

    :hammer: Tweak

--- src/components/pages/partner-institutions/universities/Universities.svelte
+++ src/components/pages/partner-institutions/universities/Universities.svelte
@@ -10,7 +10,7 @@
 
 <section>
   <div class="mx-auto max-w-[1058px] px-[20px]">
-    <h1 class="heading-2xl text-center">
+    <h2 class="heading-2xl text-center">
       <PortableText
         value={title}
         components={{
@@ -20,7 +20,7 @@
           },
         }}
       />
-    </h1>
+    </h2>
 
     <div class="my-[90px] space-y-[90px] lg:my-[146px] lg:space-y-[73px]">
       {#each universities as university}
