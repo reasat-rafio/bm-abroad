331a902fe7346d67dbd966148e1270920dec9f31
Al Reasat Rafio <rafioar96@gmail.com>
2023-10-07 15:39:35 +0600

    :iphone: Advantages page responsiveness update

--- src/components/pages/tourists-hub/advantages/Advantage.svelte
+++ src/components/pages/tourists-hub/advantages/Advantage.svelte
@@ -6,11 +6,28 @@
   export let advantage: Advantage;
   export let index: number;
   $: ({ title, image, description } = advantage);
+
+  let innerWidth = 0;
 </script>
 
-<article class="grid grid-cols-12 gap-[50px] rounded-[24px]">
-  {#if index % 2 === 0}
-    <figure class="col-span-5 p-[20px]">
+<svelte:window bind:innerWidth />
+<article
+  class="grid grid-cols-12 gap-[32px] rounded-[24px] p-[20px] md:gap-[50px] md:p-[50px]"
+>
+  {#if index % 2 === 1 && innerWidth >= 768}
+    <section
+      class="col-span-12 flex flex-col justify-center space-y-[24px] md:col-span-7"
+    >
+      <h3
+        class="text-center font-oswald text-[24px] font-semibold uppercase leading-none text-space-blue transition-colors duration-500 group-hover:text-white md:text-left md:text-[24px] xl:text-h2"
+      >
+        {title}
+      </h3>
+      <p class="body-1 hidden md:block">
+        {description}
+      </p>
+    </section>
+    <figure class="col-span-12 md:col-span-5">
       <SanityImage
         class="h-full w-full object-contain"
         sizes="20vw"
@@ -19,24 +36,8 @@
         imageUrlBuilder={imageBuilder}
       />
     </figure>
-    <section class="col-span-7 flex flex-col justify-center space-y-[24px]">
-      <h3
-        class="font-oswald text-[24px] font-semibold uppercase leading-none text-space-blue transition-colors duration-500 group-hover:text-white md:text-[24px] xl:text-h2"
-      >
-        {title}
-      </h3>
-      <p class="body-1">{description}</p>
-    </section>
   {:else}
-    <section class="col-span-7 flex flex-col justify-center space-y-[24px]">
-      <h3
-        class="font-oswald text-[24px] font-semibold uppercase leading-none text-space-blue transition-colors duration-500 group-hover:text-white md:text-[24px] xl:text-h2"
-      >
-        {title}
-      </h3>
-      <p class="body-1">{description}</p>
-    </section>
-    <figure class="col-span-5 p-[20px]">
+    <figure class="col-span-12 md:col-span-5">
       <SanityImage
         class="h-full w-full object-contain"
         sizes="20vw"
@@ -45,5 +46,17 @@
         imageUrlBuilder={imageBuilder}
       />
     </figure>
+    <section
+      class="col-span-12 flex flex-col justify-center space-y-[24px] md:col-span-7"
+    >
+      <h3
+        class="text-center font-oswald text-[24px] font-semibold uppercase leading-none text-space-blue transition-colors duration-500 group-hover:text-white md:text-left md:text-[24px] xl:text-h2"
+      >
+        {title}
+      </h3>
+      <p class="body-1 hidden md:block">
+        {description}
+      </p>
+    </section>
   {/if}
 </article>
--- src/components/pages/tourists-hub/advantages/Advantages.svelte
+++ src/components/pages/tourists-hub/advantages/Advantages.svelte
@@ -9,7 +9,9 @@
 </script>
 
 <section>
-  <div class="container space-y-[146px] pb-[146px]">
+  <div
+    class="container space-y-[90px] pb-[90px] lg:space-y-[146px] lg:pb-[146px]"
+  >
     <SanityImage
       class="aspect-video h-full max-h-[906px] w-full rounded-lg object-cover "
       lqip
@@ -19,7 +21,7 @@
       imageUrlBuilder={imageBuilder}
     />
     <h2 class="heading-2xl text-center">{title}</h2>
-    <div class="mx-auto max-w-[958px] space-y-[63px]">
+    <div class="mx-auto max-w-[1058px] space-y-[63px]">
       {#each advantages as advantage, index}
         <Advantage {advantage} {index} />
       {/each}
