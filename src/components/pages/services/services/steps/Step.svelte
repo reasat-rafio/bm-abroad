c6bc324f4d210395d7c69fe47ad275429182703f
Al Reasat Rafio <rafioar96@gmail.com>
2023-10-15 20:30:19 +0600

    :iphone: Steps responsiveness

--- src/components/pages/services/services/steps/Step.svelte
+++ src/components/pages/services/services/steps/Step.svelte
@@ -11,6 +11,7 @@
   let scrollPercentage = 0;
   let stepEl: HTMLDivElement;
   let heraderEl: HTMLHeadElement;
+  let innerWidth = 0;
   $: headerDeltaY =
     heraderEl?.getBoundingClientRect().top -
     stepEl?.getBoundingClientRect().top;
@@ -42,13 +43,82 @@
 
 <svelte:window
   bind:innerHeight={windowHeight}
+  bind:innerWidth
   on:scroll={scrollAction}
   on:resize={calcDeltaY}
 />
-<div bind:this={stepEl} class="grid grid-cols-11 gap-[20px] !overflow-visible">
-  {#if index % 2 === 0}
+{#if innerWidth >= 1024}
+  <div
+    bind:this={stepEl}
+    class="grid grid-cols-11 gap-[20px] !overflow-visible"
+  >
+    {#if index % 2 === 0}
+      <div
+        class="col-span-5 space-y-[2px] pb-[65px] text-right lg:pb-[150px] {isIntersecting
+          ? 'scale-100'
+          : 'scale-95'} transition-transform duration-500 ease-in-out"
+      >
+        <div
+          class="{textColor(
+            'text-dark-gray',
+          )} text-[12px] uppercase transition-colors duration-500 lg:text-[16px]"
+        >
+          Step {index + 1} :
+        </div>
+
+        <h4
+          bind:this={heraderEl}
+          class="{textColor()} heading-xl transition-colors duration-500"
+        >
+          {title}
+        </h4>
+
+        <p class={twMerge(`${textColor()} body-2`, 'text-[12px]')}>
+          {description}
+        </p>
+      </div>
+    {:else}
+      <div class="col-span-5" />
+    {/if}
+
+    <Indicator {headerDeltaY} {isIntersecting} {isLast} {scrollPercentage} />
+    {#if index % 2 !== 0}
+      <div
+        class="col-span-5 space-y-[2px] pb-[65px] text-left lg:pb-[150px] {isIntersecting
+          ? 'scale-100'
+          : 'scale-95'} transition-transform duration-500 ease-in-out"
+      >
+        <div
+          class="{textColor(
+            'text-dark-gray',
+          )} text-[12px] uppercase transition-colors duration-500 lg:text-[16px]"
+        >
+          Step {index + 1} :
+        </div>
+
+        <h4
+          bind:this={heraderEl}
+          class="{textColor()} heading-xl transition-colors duration-500"
+        >
+          {title}
+        </h4>
+
+        <p class={twMerge(`${textColor()} body-2`, 'text-[12px]')}>
+          {description}
+        </p>
+      </div>
+    {:else}
+      <div class="col-span-5" />
+    {/if}
+  </div>
+{:else}
+  <div
+    bind:this={stepEl}
+    class="grid grid-cols-12 gap-[20px] !overflow-visible"
+  >
+    <Indicator {headerDeltaY} {isIntersecting} {isLast} {scrollPercentage} />
     <div
-      class="col-span-5 space-y-[2px] pb-[65px] text-right lg:pb-[150px] {isIntersecting
+      class="col-span-11 space-y-[2px] pb-[65px] lg:pb-[150px] {isIntersecting
         ? 'scale-100'
         : 'scale-95'} transition-transform duration-500 ease-in-out"
     >
@@ -71,37 +141,5 @@
         {description}
       </p>
     </div>
-  {:else}
-    <div class="col-span-5" />
-  {/if}
-
-  <Indicator {headerDeltaY} {isIntersecting} {isLast} {scrollPercentage} />
-  {#if index % 2 !== 0}
-    <div
-      class="col-span-5 space-y-[2px] pb-[65px] text-left lg:pb-[150px] {isIntersecting
-        ? 'scale-100'
-        : 'scale-95'} transition-transform duration-500 ease-in-out"
-    >
-      <div
-        class="{textColor(
-          'text-dark-gray',
-        )} text-[12px] uppercase transition-colors duration-500 lg:text-[16px]"
-      >
-        Step {index + 1} :
-      </div>
-
-      <h4
-        bind:this={heraderEl}
-        class="{textColor()} heading-xl transition-colors duration-500"
-      >
-        {title}
-      </h4>
-
-      <p class={twMerge(`${textColor()} body-2`, 'text-[12px]')}>
-        {description}
-      </p>
-    </div>
-  {:else}
-    <div class="col-span-5" />
-  {/if}
-</div>
+  </div>
+{/if}
