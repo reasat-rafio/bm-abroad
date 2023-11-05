505e02b596dc2e61e46338197e1836579443ab1f
reasat-rafio <rafioar96@gmail.com>
2023-11-05 09:24:40 +0600

    :rocket: Config changed to isr rendering and edge depoloyment

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,12 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({}),
+    adapter: adapter({
+      runtime: "edge",
+      isr: {
+        expiration: 120,
+      }
+    }),
     alias: {
       '@/*': 'src/*',
     },
