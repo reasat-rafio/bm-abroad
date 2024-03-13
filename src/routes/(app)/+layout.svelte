09661b200a0f5704baff62a269092addfce90285
reasat-rafio <rafioar96@gmail.com>
2024-03-13 13:33:40 +0600

    ascii art added

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -21,6 +21,19 @@
   $: faviconImage = favicon
     ? urlFor(favicon).format('png').size(256, 256).ignoreImageParams().url()
     : null;
+
+  const asciiArtEdistys = `
+  ______    _ _     _                       _
+ |  ____|  | (_)   | |                     | |
+ | |__   __| |_ ___| |_ _   _ ___        __| | _____   __
+ |  __| / _\` | / __| __| | | / __|      / _\` |/ _ \\ \\ / /
+ | |___| (_| | \\__ \\ |_| |_| \\__ \\  _  | (_| |  __/\\ V /
+ |______\\__,_|_|___/\\__|\\__, |___/ (_)  \\__,_|\\___| \\_/
+                         __/ |
+                        |___/
+`;
+
+  console.log(asciiArtEdistys);
 </script>
 
 <svelte:head>
